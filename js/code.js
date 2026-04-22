e62b9163980f329925291fbfe69bb5fb7efda834
Java <73376510+Javascript-void0@users.noreply.github.com>
2026-04-21 20:55:29 -0400

    project 4

---
+++ js/code.js
@@ -0,0 +1,16 @@
+// import('../assets/highlight/es/core.js').then((hljs) => {
+//     import('../assets/highlight/es/languages/python.min.js').then((python) => {
+//         hljs.registerLanguage('javascript', python);
+//     })
+// })
+// import javascript from 'assets/highlight/es/languages/javascript.min.js';
+import '../assets/highlight/es/core.js'
+
+
+
+// hljs.registerLanguage('javascript', javascript);
+
+// console.log('weofijwoeifj')
+
+
+// hljs.registerLanguage('go', go);

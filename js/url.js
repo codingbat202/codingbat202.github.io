ae421c7e7206cac7a0bc18190f8cdadae4328b4c
Java <73376510+Javascript-void0@users.noreply.github.com>
2026-04-27 14:06:04 -0400

    w

--- js/url.js
+++ js/url.js
@@ -5,7 +5,26 @@ function goUrl(newurl) {
     var url = new URL(url_string);
     var loggedIn = url.searchParams.get("login");
 
+    if (newurl == '/login/') { newurl += '?prev=' + window.location.pathname }
+
     if (loggedIn != null) { newurl += '?login=' }
 
     window.location.href=newurl
+}
+
+
+function goPrevLoggedIn() {
+    var url_string = window.location;
+    var url = new URL(url_string);
+    var prevUrl = url.searchParams.get("prev");
+
+    if (prevUrl != null) {
+        window.location.href = prevUrl + '?login='
+    } else {
+        window.location.href = '/?login='
+    }
+}
+
+function signOutUrl() {
+    window.location.href = window.location.pathname;
 }
